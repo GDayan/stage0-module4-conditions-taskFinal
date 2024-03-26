0662ba430178bee0e5668c73cc4b54e6749fe0ae
GDayan <daykanot@gmail.com>
2024-03-27 00:46:00 +0300

    done conditions task final

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
+        System.out.println("Before swapping: first=" + first + ", second=" + second);
+        first ^= second;
+        second ^= first;
+        first ^= second;
+        System.out.println("After swapping: first=" + first + ", second=" + second);
     }
 }
+
